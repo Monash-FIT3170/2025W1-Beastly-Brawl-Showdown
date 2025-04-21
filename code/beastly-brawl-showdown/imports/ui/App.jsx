0d49c0dce87f5d620f5f96494d9fcccdb5fb1bbd
Mark Shim Wen Ren <Mshi0073@student.monash.edu>
2025-04-21 18:54:40 +1000

    added some simple front end for host join screen

--- code/beastly-brawl-showdown/imports/ui/App.jsx
+++ code/beastly-brawl-showdown/imports/ui/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import { Host } from './Host';
+import { JoinForm } from './JoinForm';
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
+    <Host />
+    <JoinForm />
   </div>
 );
