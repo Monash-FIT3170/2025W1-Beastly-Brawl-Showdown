4c70eb635a2007b7370349ef07477f6c202f9447
Joesiah <tsunetoishida03@gmail.com>
2025-04-22 19:56:43 +1000

    Created component to show the room link/code

---
+++ code/beastly-brawl-showdown/imports/ui/CodeLink.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+export const CodeLink = () => {
+    return (
+        <div class="code-link">
+            BeastlyBrawlShowdown.com/69420
+        </div>
+    )
+}
