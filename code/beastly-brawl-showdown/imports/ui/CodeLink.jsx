91933ef4cfee204cdbeeb574d83eef9aec942167
Joesiah <tsunetoishida03@gmail.com>
2025-04-22 20:50:39 +1000

    Created Component to show generated QR code

--- code/beastly-brawl-showdown/imports/ui/CodeLink.jsx
+++ code/beastly-brawl-showdown/imports/ui/CodeLink.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 export const CodeLink = () => {
     return (
-        <div class="code-link">
+        <div className="code-link">
             BeastlyBrawlShowdown.com/69420
         </div>
     )
