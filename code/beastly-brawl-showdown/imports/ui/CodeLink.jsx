a0128a134ae3716dfca2c032d0e2b3e38c75825d
Joesiah <tsunetoishida03@gmail.com>
2025-04-22 21:06:58 +1000

    Implemented Component to store link/code and qrcode components

--- code/beastly-brawl-showdown/imports/ui/CodeLink.jsx
+++ code/beastly-brawl-showdown/imports/ui/CodeLink.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 export const CodeLink = () => {
     return (
         <div className="code-link">
-            BeastlyBrawlShowdown.com/69420
+            BeastlyBrawlShowdown.com/69420testtest
         </div>
     )
 }
