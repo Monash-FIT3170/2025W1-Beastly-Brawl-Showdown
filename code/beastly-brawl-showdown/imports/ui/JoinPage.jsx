7ffc47a4525e6160f891d7e88ef7b2c495cb95a4
Rohan Kannan <2rohankannan@gmail.com>
2025-04-25 14:20:47 +1000

    added NamePage, and clicking Continue moves between pages

--- code/beastly-brawl-showdown/imports/ui/JoinPage.jsx
+++ code/beastly-brawl-showdown/imports/ui/JoinPage.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "/imports/ui/global.css";
 
 export default function JoinPage() {
+  const navigate = useNavigate();
   return (
     <div className="page-container">
       <h1>Join Page</h1>
+      <div className="buttons-container">
+        <button className="btn name-btn" onClick={() => navigate("/name")}>
+          CONTINUE
+        </button>
+      </div>
     </div>
   );
 }
