1c36ec6a2f735129a29835436d7b2598228c0856
Rohan Kannan <2rohankannan@gmail.com>
2025-04-25 15:08:05 +1000

    JoinForm added to JoinPage

--- code/beastly-brawl-showdown/imports/ui/JoinPage.jsx
+++ code/beastly-brawl-showdown/imports/ui/JoinPage.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {useref} from "react";
 import { useNavigate } from "react-router-dom";
 import "/imports/ui/global.css";
+import { JoinForm } from "./JoinForm";
 
 export default function JoinPage() {
   const navigate = useNavigate();
+
+  // const formRef = useRef();
+
+  // // Function to trigger form submission
+  // const handleFormSubmit = () => {
+  //   if (formRef.current) {
+  //     formRef.current.requestSubmit(); // This triggers the form's submit event
+  //   }
+  // };
+
   return (
     <div className="page-container">
       <h1>Join Page</h1>
+      <JoinForm/>
+      {/* <JoinForm ref = {formRef}/> */}
+      {/* have a task form that takes room code using marks */}
       <div className="buttons-container">
         <button className="btn name-btn" onClick={() => navigate("/name")}>
           CONTINUE
