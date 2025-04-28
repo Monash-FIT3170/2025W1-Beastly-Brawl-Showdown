6d4729b2cfbbef57373aa40e1a228dc27a5b1690
Rohan Kannan <2rohankannan@gmail.com>
2025-04-29 09:24:19 +1000

    Added NameForm for inputting name, need to do functionality and css

--- code/beastly-brawl-showdown/imports/ui/JoinPage.jsx
+++ code/beastly-brawl-showdown/imports/ui/JoinPage.jsx
@@ -1,4 +1,4 @@
-import React, {useref} from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import "/imports/ui/global.css";
 import { JoinForm } from "./JoinForm";
