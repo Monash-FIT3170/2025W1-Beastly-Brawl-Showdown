dbb9f166e8893128e2704f7d7ff6bb5dc19b487b
Jamie N. Kaltzis <jkal0010@student.monash.edu>
2025-04-30 12:00:42 +1000

    Add custom attack method - Class has an attribute rerollCharge initialised as true, can only be used once. - Attack tells the user the roll and gives the option to reroll if the charge has not already been consumed. Calculates totalAttack using the last roll.

--- data/monsters/MysticWyvern.js
+++ data/monsters/MysticWyvern.js
@@ -1,17 +1,35 @@
 const Monsters = require('./Monsters');
+const DiceRoller = require('../utils/DiceRoller');
+
 
 class MysticWyvern extends Monsters {
     constructor() {
         super(25, 14, 2, "None","Balanced");
+        rerollCharge = true;
     }
 
-    activateSpecial() {
+    activateSpecial() {}
 
-    }
+    activateAbility() {}
 
-    // Method for abilities
-    activateAbility() {
+    attack(defender) {
+        let roll = DiceRoller.d20();
+        console.log(`${this._type} rolls ${roll}.`);
 
+        // Ask the user if they want to reroll
+        if (rerollCharge) {
+            var reroll = confirm("Do you want to reroll the d20?");
+        }
+        
+        if (reroll) {
+            roll = DiceRoller.d20();
+            console.log(`${this._type} rerolls and gets ${roll}.`);
+        }
+
+        const totalAttack = roll + this._attackBonus;
+        console.log(`${this._type} rolls ${roll}... Attack = ${totalAttack}.`);
+
+        defender.defend(totalAttack);
     }
 }
 
