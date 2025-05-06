9932a472da962db13704d5428d89497335992b76
Jamie N. Kaltzis <jkal0010@student.monash.edu>
2025-05-07 09:27:53 +1000

    Remove ability - Does not function correctly as revert defense is not called unless the player selects the defend action. - This means the invulnerable status would be kept until the next time the player defends.

--- data/monsters/ShadowFangPredator.ts
+++ data/monsters/ShadowFangPredator.ts
@@ -6,7 +6,6 @@ class ShadowFangPredator extends Monsters {
     constructor() {
         super(20, 12, 4, "Critical hit on natural 18-20","Attacker");
         this._critRoll = 18;
-        this._invulnerable = false;
     }
 
     activateSpecial() {}
@@ -29,29 +28,9 @@ class ShadowFangPredator extends Monsters {
         defender.defend(totalAttack)
     }
 
-    defend(totalAttack) {
-        if (this._invulnerable) {
-            console.log(`${this.type} evades the attack!`);
-        } else {
-            super.defend(totalAttack)
-        }
-    }
-
-    // Shadow Leap: Evades an attack.
-    ability(defender) {
-        this._invulnerable = true;
-        this._abilityCharge -= 1;
-    }
-
     revertDefense() {
-        if (defending) {
-            this._defending = false;
-            this._currentAC = this._baseAC;
-        }
-
-        if (invulnerable) {
-            this._invulnerable = false;
-        }
+        this._defending = false;
+        this._currentAC = this._baseAC;
     }
 }
 
