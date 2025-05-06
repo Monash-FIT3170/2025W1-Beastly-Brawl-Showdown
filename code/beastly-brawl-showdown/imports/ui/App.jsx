bb28668c0edf9f993bb24a2e9278fe2b24cf9cb8
weihom05 <wtan0125@student.monash.edu>
2025-05-06 21:22:39 +1000

    Deleted HostPage and link WaitingRoom to HomePage

--- code/beastly-brawl-showdown/imports/ui/App.jsx
+++ code/beastly-brawl-showdown/imports/ui/App.jsx
@@ -6,3 +6,22 @@
 //     <WaitingRoom />
 //   </div>
 // )
+
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./HomePage";
+import JoinPage from "./JoinPage";
+import NamePage from "./NamePage";
+import { WaitingRoom } from "./WaitingRoom";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/host" element={<WaitingRoom />} />
+      <Route path="/join" element={<JoinPage />} />
+      <Route path="/name" element={<NamePage />} />
+    </Routes>
+  );
+}
