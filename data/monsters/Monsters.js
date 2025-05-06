e295bb2ff968e45b4db69303a7ec512fd00a14c1
Jamie N. Kaltzis <jkal0010@student.monash.edu>
2025-05-06 15:50:02 +1000

    Add current AC and base AC for reverting defense rather than relying on hardcoded values.

--- data/monsters/Monsters.js
+++ data/monsters/Monsters.js
@@ -3,53 +3,14 @@ const DiceRoller = require('../utils/DiceRoller');
 class Monsters {
     constructor(health, AC, attackBonus, special, type) {
         this._health = health;
-        this._AC = AC;
+        this._baseAC = AC;
+        this._currentAC = AC;
         this._attackBonus = attackBonus;
-        this.special = special;
+        this._special = special;
         this._type = type;
         this._defenseCharges = 3;
         this._defending = false;
     }
-
-    // Getter and Setter for health
-    get health() {
-        return this._health;
-    }
-
-    set health(value) {
-        this._health = value;
-    }
-
-    // Getter and Setter for AC
-    get AC() {
-        return this._AC;
-    }
-
-    set AC(value) {
-        this._AC = value;
-    }
-
-    // Getter and Setter for attackBonus
-    get attackBonus() {
-        return this._attackBonus;
-    }
-
-    set attackBonus(value) {
-        this._attackBonus = value;
-    }
-
-    // Getter for type
-    get type() {
-        return this._type;
-    }
-
-    // Method to activate special
-    activateSpecial() { }
-
-    // Method for abilities
-    activateAbility_1() { }
-
-    activateAbility_2() { }
 
     // Method for Activiting Defending
     activateDefense() {
@@ -92,12 +53,46 @@ class Monsters {
         else {
             if (totalAttack >= this._currentAC) {
                 console.log(`${this.type}' takes ${totalAttack} damage!`);
-                this._currentHealth -= totalAttack;
+                this._health -= totalAttack;
             }
             else {
                 console.log(`${this.type}' the attack misses!`);
             }
         }
+    }
+
+    ability(attacker, defender) {}
+
+    // Getter and Setter for health
+    get health() {
+        return this._health;
+    }
+
+    set health(value) {
+        this._health = value;
+    }
+
+    // Getter and Setter for AC
+    get AC() {
+        return this._AC;
+    }
+
+    set AC(value) {
+        this._AC = value;
+    }
+
+    // Getter and Setter for attackBonus
+    get attackBonus() {
+        return this._attackBonus;
+    }
+
+    set attackBonus(value) {
+        this._attackBonus = value;
+    }
+
+    // Getter for type
+    get type() {
+        return this._type;
     }
 }
 
