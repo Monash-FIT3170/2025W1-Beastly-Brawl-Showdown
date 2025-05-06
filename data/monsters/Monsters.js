37482a5873dc5fa8474b326ae6ffb3203aef3c5c
Jamie N. Kaltzis <jkal0010@student.monash.edu>
2025-05-06 15:50:02 +1000

    Add current AC and base AC for reverting defense rather than relying on hardcoded values.

--- data/monsters/Monsters.js
+++ data/monsters/Monsters.js
@@ -3,13 +3,65 @@ const DiceRoller = require('../utils/DiceRoller');
 class Monsters {
     constructor(health, AC, attackBonus, special, type) {
         this._health = health;
-        this._AC = AC;
+        this._baseAC = AC;
+        this._currentAC = AC;
         this._attackBonus = attackBonus;
-        this.special = special;
+        this._special = special;
         this._type = type;
         this._defenseCharges = 3;
         this._defending = false;
     }
+
+    // Method for Activiting Defending
+    activateDefense() {
+        this._defending = true;
+        this._currentAC = this._baseAC + 2
+        this._defenseCharges -= 1;
+    }
+
+    revertDefense() {
+        if (defending) {
+            this._defending = false;
+            this._currentAC = this._baseAC;
+        }
+    }
+
+    // Method for Monster to Attack
+    attack(defender) {
+        const roll = DiceRoller.d20();
+        const totalAttack = roll + this._attackBonus;
+        console.log(`${this.type} rolls ${roll}... Attack = ${totalAttack}.`);   
+        return totalAttack;
+    }
+
+    // Method for Monster to Defend
+    defend(totalAttack) {
+        if (this._defending = true) {
+
+            if (totalAttack >= this._AC) {
+                console.log(`${this.type}' defense fails! ${this.type} takes 5 damage.`)
+                this._health -= 5
+            }
+            else {
+                console.log(`${this.type}' the attack misses!`);
+            }
+
+            this._AC -= 2
+            this._defending = false;
+            return;
+        }
+        else {
+            if (totalAttack >= this._currentAC) {
+                console.log(`${this.type}' takes ${totalAttack} damage!`);
+                this._health -= totalAttack;
+            }
+            else {
+                console.log(`${this.type}' the attack misses!`);
+            }
+        }
+    }
+
+    ability(attacker, defender) {}
 
     // Getter and Setter for health
     get health() {
@@ -41,65 +93,6 @@ class Monsters {
     // Getter for type
     get type() {
         return this._type;
-    }
-
-    // Method to activate special
-    activateSpecial() { }
-
-    // Method for abilities
-    activateAbility_1() { }
-
-    activateAbility_2() { }
-
-    // Method for Activiting Defending
-    activateDefense() {
-        if (this._defenseCharges > 0) {
-            this._defending = true;
-            this._AC += 2
-            this._defenseCharges -= 1
-        }
-        else {
-            console.log(`${this.type} has no defense charges remaining!`)
-        }
-    }
-
-    // Method for Monster to Attack
-    attack(defender) {
-        const roll = DiceRoller.d20();
-        const totalAttack = roll + this._attackBonus;
-        console.log(`${this.type} rolls ${roll}... Attack = ${totalAttack}.`)
-
-        defender.defend(totalAttack)
-    }
-
-    // Method for Monster to Defend
-    defend(totalAttack) {
-        if (this._defending = true) {
-
-            if (totalAttack >= this._AC) {
-                console.log(`${this.type}' defense fails! ${this.type} takes 5 damage.`)
-                this._health -= 5
-            }
-
-            else {
-                console.log(`${this.type}' the attack misses!`)
-            }
-
-            this._AC -= 2
-            this._defending = false;
-            return;
-        }
-
-        else {
-            if (totalAttack >= this._AC) {
-                console.log(`${this.type}' takes ${totalAttack} damage!`)
-                this._health -= totalAttack
-            }
-
-            else {
-                console.log(`${this.type}' the attack misses!`)
-            }
-        }
     }
 }
 
