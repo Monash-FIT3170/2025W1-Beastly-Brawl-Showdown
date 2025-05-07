56ecaec62971be22b445da7cca9b1a4d9189db35
Jamie N. Kaltzis <jkal0010@student.monash.edu>
2025-05-07 10:45:02 +1000

    Implement useAbility method. - Stuns the defending monster.

--- data/monsters/StoneHideGuardian.js
+++ data/monsters/StoneHideGuardian.js
@@ -7,6 +7,11 @@ class StoneHideGuardian extends Monsters {
         super(30, 16, 1, "4 defense actions per battle","Defender");
         this.defenseCharges = 4;
     }
+
+    useAbility(defender) {
+        this._currentAbilityCharges -= 1;
+        defender.stun();
+    }
 }
 
 module.exports = StoneHideGuardian;
