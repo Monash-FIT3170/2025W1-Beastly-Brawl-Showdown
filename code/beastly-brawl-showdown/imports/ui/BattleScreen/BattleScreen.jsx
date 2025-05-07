f5761129e397c014ccc9202b13c866fc5d7ceed2
Mark Shim Wen Ren <Mshi0073@student.monash.edu>
2025-05-08 03:43:32 +1000

    added the ability button and also the roll animaiton

--- code/beastly-brawl-showdown/imports/ui/BattleScreen/BattleScreen.jsx
+++ code/beastly-brawl-showdown/imports/ui/BattleScreen/BattleScreen.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BattleTop } from './BattleTop.jsx';
 import { BattleMiddle } from './BattleMiddle.jsx';
 import { BattleBottom } from './BattleBottom.jsx';
 
-export const BattleScreen = () => (
-  <div className = "battleScreen">
-    <BattleTop/>
-    <BattleMiddle/>
-    <BattleBottom/>
-  </div>
-);
+export const BattleScreen = () => {
+  const [showAnimation, setShowAnimation] = useState(false);
+
+  //function to trigger the rolling animation, but i'm genuinely not sure why the rolling animation spazzes out, but on the bright side, it looks like its rolling ig
+  const triggerAnimation = () => {
+    setShowAnimation(true);
+    setTimeout(() => setShowAnimation(false), 3000); 
+  };
+
+  return (
+    <div className="battleScreen">
+      <BattleTop />
+      <BattleMiddle showAnimation={showAnimation} />
+      <BattleBottom onRoll={triggerAnimation} />
+    </div>
+  );
+};
