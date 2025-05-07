3aca43439a8aaca6d46f368b033c6779953f1a7f
Mark Shim Wen Ren <Mshi0073@student.monash.edu>
2025-05-08 03:48:00 +1000

    fixed a bug regarding the dice rolls animation

--- code/beastly-brawl-showdown/imports/ui/BattleScreen/BattleScreen.jsx
+++ code/beastly-brawl-showdown/imports/ui/BattleScreen/BattleScreen.jsx
@@ -8,8 +8,10 @@ export const BattleScreen = () => {
 
   //function to trigger the rolling animation, but i'm genuinely not sure why the rolling animation spazzes out, but on the bright side, it looks like its rolling ig
   const triggerAnimation = () => {
+    if(showAnimation == false){
     setShowAnimation(true);
     setTimeout(() => setShowAnimation(false), 3000); 
+    }
   };
 
   return (
