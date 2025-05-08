b798ae274594a0347f1fec258b8a6c951cda0187
Jonathan Foo <jonny.foo@outlook.com>
2025-05-09 02:03:04 +1000

    Monster border onclick and hover effect added

--- code/beastly-brawl-showdown/imports/ui/MonsterSelection/MonsterContainer.jsx
+++ code/beastly-brawl-showdown/imports/ui/MonsterSelection/MonsterContainer.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
-export const MonsterContainer = ({image, name}) => {
+export const MonsterContainer = ({image, name, func}) => {
+    // Onclick handler
+    function onClick() {
+        func(name);
+    }
+    // // Dictate appearance on mouse hover
+    // function onMouseOver() {
+    //     let style = document.getElementById(name).style;
+    //     style.border = "solid";
+    //     style.borderWidth = "5px";
+    //     style.borderColor = "black";
+    // }
+
+    // // Undo appearance change when mouse leaves
+    // function onMouseLeave() {
+    //     let style = document.getElementById(name).style;
+    //     style.border = "none";
+    // }
+
     return (
         <div class="monsterContainer">
-            <img src={image} id={name} class="monster"/>
+            <img src={image} id={name} class="monsterImage" onClick={onClick}/>
         </div>
     )
 }   
--- code/beastly-brawl-showdown/imports/ui/MonsterSelection/MonsterSelectionScreen.jsx
+++ code/beastly-brawl-showdown/imports/ui/MonsterSelection/MonsterSelectionScreen.jsx
@@ -2,12 +2,29 @@ import React from "react";
 import { MonsterContainer } from "./MonsterContainer";
 
 export const MonsterSelectionScreen = () => {
+    // Name of currently selected monster
+    let currentlySelected; 
+
+    // On selection, display confirm button and border the selected monster
+    function highlightAndShowConfirm(name) {
+        if (currentlySelected) {
+            // Deselect currently selected item
+            let deselect = document.getElementById(currentlySelected).style;
+            deselect.border = "none"
+        }
+
+        let style = document.getElementById(name).style;
+        style.border = "solid"
+        style.borderWidth = "8px";
+        currentlySelected = name;
+    }
+
     return (
         <div class="monsterSelectionScreen">
-            <h1>TEST SCRIPT</h1>
-            <MonsterContainer image="img/placeholder_monster_1.png" name="monster1"/>
-            <MonsterContainer image="img/placeholder_monster_2.png" name="monster2"/>
-            <MonsterContainer image="img/placeholder_monster_3.png" name="monster3"/>
+            <h1>Choose your Monster:</h1>
+            <MonsterContainer image="img/placeholder_monster_1.png" name="monster1" func={highlightAndShowConfirm}/>
+            <MonsterContainer image="img/placeholder_monster_2.png" name="monster2" func={highlightAndShowConfirm}/>
+            <MonsterContainer image="img/placeholder_monster_3.png" name="monster3" func={highlightAndShowConfirm}/>
         </div>
     )
-}
+} 
