c6a1d9dbabee082fa972adc9be5a616388726e83
Joesiah <tsunetoishida03@gmail.com>
2025-05-12 01:37:40 +1000

    Added basic structure to the design systems page - incomplete

--- code/beastly-brawl-showdown/imports/ui/DesignSystem.jsx
+++ code/beastly-brawl-showdown/imports/ui/DesignSystem.jsx
@@ -1,14 +1,36 @@
 import React, { useState } from 'react';
-import { DisplayPanel } from './DisplayPanel';
-import { TallPanels } from './TallPanels';
-import { Buttons } from './Buttons';
 
 export const DesignSystem = () => {
     return (
         <div className='design-system-page'>
-            <DisplayPanel />
-            <TallPanels />
-            <Buttons />
+            <div className="branding">
+                <div className="title">Design</div>
+                <div className="subtitle">System</div>
+                <div className='team-name'>KahootLite</div>
+            </div>
+
+            <div className="icons">
+                <div className="icons-header">Icons</div>
+                <div className="icon-grid"></div>
+            </div>
+
+            <div className="patterns-top">
+                <div className="patterns-header">Patterns</div>
+            </div>
+
+            <div className='typography'>
+                <div className='typography-header'>Typography</div>
+                <div className="typography-sample">
+                    <div className="sample-box">
+                        <div className="font-size: 5rem; line-height: 1;">Aa</div>
+                    </div>
+                    <div className="type-examples">
+                        <div className="type-large">Title Large Meta</div>
+                        <div className="type-medium">Title Medium Meta</div>
+                        <div className="type-small">Title Small Meta</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
