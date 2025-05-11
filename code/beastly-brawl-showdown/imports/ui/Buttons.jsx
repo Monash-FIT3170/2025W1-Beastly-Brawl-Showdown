21b6ca11b0b23ce7016bf9aac4ab034e5e2ad4fc
Joesiah <tsunetoishida03@gmail.com>
2025-05-12 01:09:14 +1000

    Removed old files

--- code/beastly-brawl-showdown/imports/ui/Buttons.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-
-export const Buttons = () => {
-    return (
-        <div className='panels'>
-            <div className='play-button'></div>
-        </div>
-    )
-}
--- code/beastly-brawl-showdown/imports/ui/PlayerName.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react';
-
-export const PlayerName = () => {
-    return (
-        <div className='player-tag'></div>
-    )
-}
