c8e6047b2cf55c6d50506a7662a7cedab9d636a4
Mark Shim Wen Ren <Mshi0073@student.monash.edu>
2025-05-12 00:15:32 +1000

    made very initial loging page

---
+++ code/beastly-brawl-showdown/imports/ui/Login/LoginPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { UserForm } from "./UserForm";
+
+export const LoginPage = () => {
+
+  return (
+    <UserForm/>
+  );
+};
---
+++ code/beastly-brawl-showdown/imports/ui/Login/UserForm.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+export const UserForm = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassWord] = useState("");
+
+  // todo: make this login the user or smth
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!userName) return;
+
+    // Replace this with real login logic
+    console.log("Logging in with:", { userName, password });
+
+    setUserName("");
+    setPassWord("");
+  };
+
+  return (
+    <form className="userForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="username"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="password, leave blank for guest"
+        value={password}
+        onChange={(e) => setPassWord(e.target.value)}
+      />
+      
+      <button type="submit">Login</button>
+      <button>Sign Up</button>
+    </form>
+  );
+};
