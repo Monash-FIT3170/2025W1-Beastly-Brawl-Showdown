a59e406854aa0f5ccd26988fc7ead13ceac004e2
Jonathan Foo <jonny.foo@outlook.com>
2025-05-11 16:49:16 +1000

    Display for monsters finished with comments

--- code/beastly-brawl-showdown/imports/ui/MonsterSelection/MonsterContainer.jsx
+++ code/beastly-brawl-showdown/imports/ui/MonsterSelection/MonsterContainer.jsx
@@ -1,23 +1,12 @@
 import React from "react";
 
 export const MonsterContainer = ({image, name, func}) => {
-    // Onclick handler
+    /**
+     * Onclick handler
+     */
     function onClick() {
         func(name);
     }
-    // // Dictate appearance on mouse hover
-    // function onMouseOver() {
-    //     let style = document.getElementById(name).style;
-    //     style.border = "solid";
-    //     style.borderWidth = "5px";
-    //     style.borderColor = "black";
-    // }
-
-    // // Undo appearance change when mouse leaves
-    // function onMouseLeave() {
-    //     let style = document.getElementById(name).style;
-    //     style.border = "none";
-    // }
 
     return (
         <div class="monsterContainer">
--- code/beastly-brawl-showdown/imports/ui/MonsterSelection/MonsterSelectionScreen.jsx
+++ code/beastly-brawl-showdown/imports/ui/MonsterSelection/MonsterSelectionScreen.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import { MonsterContainer } from "./MonsterContainer";
 
+/**
+ * Confirms the monster the player will be using and all function that this button needs
+ * @returns HTML button component to confirm monster selection
+ */
+function ConfirmButton() {
+    return (
+        <button id="confirmMonsterButton" disabled>
+            Confirm
+        </button>
+    )
+}
+
 export const MonsterSelectionScreen = () => {
     // Name of currently selected monster
     let currentlySelected; 
 
-    // On selection, display confirm button and border the selected monster
+    /**
+     * Enable confirm button and border the selected monster
+     * @param {string} name 
+     */
     function highlightAndShowConfirm(name) {
         if (currentlySelected) {
             // Deselect currently selected item
@@ -13,10 +28,16 @@ export const MonsterSelectionScreen = () => {
             deselect.border = "none"
         }
 
+        // Create border around selected mosnter
         let style = document.getElementById(name).style;
         style.border = "solid"
         style.borderWidth = "8px";
         currentlySelected = name;
+
+        // Enable confirm button once an option has been selected
+        let confirmButton = document.getElementById("confirmMonsterButton");
+        confirmButton.disabled = false;
+        confirmButton.style.cursor = "default";
     }
 
     return (
@@ -25,6 +46,8 @@ export const MonsterSelectionScreen = () => {
             <MonsterContainer image="img/placeholder_monster_1.png" name="monster1" func={highlightAndShowConfirm}/>
             <MonsterContainer image="img/placeholder_monster_2.png" name="monster2" func={highlightAndShowConfirm}/>
             <MonsterContainer image="img/placeholder_monster_3.png" name="monster3" func={highlightAndShowConfirm}/>
+            <ConfirmButton/>
         </div>
     )
-} 
+}
+
