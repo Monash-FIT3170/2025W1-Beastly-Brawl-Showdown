1911f857f6039f30cbdc88a2048284a07fabc915
Harshil <iamharshilpatelfrommars@gmail.com>
2025-05-13 15:12:34 +1200

    Updated HostRoom routes with name

--- code/beastly-brawl-showdown/imports/ui/Room/HostRoomPage.jsx
+++ code/beastly-brawl-showdown/imports/ui/Room/HostRoomPage.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect } from "react";
 import { Meteor } from "meteor/meteor";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const HostRoomPage = () => {
   const navigate = useNavigate();
+
+  //get name for the person whos hosting it
+  const { name } = useParams();
+  const playerName = decodeURIComponent(name);
+
   const onRequestRoom = () => {
     console.log("Requesting room...");
     Meteor.call("requestNewRoom", (error, result) => {
@@ -14,7 +19,8 @@ export const HostRoomPage = () => {
 
       console.log("Room created with result:", result);
       console.log(`Moving host to room #${result.roomCode}`);
-      navigate(`/h/${result.roomCode}`);
+      navigate(`/h/${result.roomCode}/${encodeURIComponent(playerName)}`);
+
     });
   };
 
