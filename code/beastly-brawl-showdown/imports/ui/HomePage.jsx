338628327368a5e9d508a132c1cb8f36512aabe1
Harshil <iamharshilpatelfrommars@gmail.com>
2025-05-13 15:12:12 +1200

    Updated HomePage to handle guestName

--- code/beastly-brawl-showdown/imports/ui/HomePage.jsx
+++ code/beastly-brawl-showdown/imports/ui/HomePage.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export const HomePage = () => {
+  //pass the name of the guest person 
   const navigate = useNavigate();
+  const guestName = sessionStorage.getItem("guestName");
+
+  // go to the host room setup with the guests name in the URL
+  const handleGuestHostName = () => {
+    navigate(`/host/${encodeURIComponent(guestName)}`);
+  };
   return (
     <>
       <h1>Home</h1>
-
+      <h1>Welcome {guestName}!</h1>
       <div>
-        <button onClick={() => navigate("/host")}>Host Room</button>
+        <button onClick={handleGuestHostName}>Host Room</button>
         <button onClick={() => navigate("/join")}>Join Room</button>
       </div>
     </>
