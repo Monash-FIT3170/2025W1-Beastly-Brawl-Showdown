0c36f858af06b65f3bff1ec1d277f1c890699ab3
Mechanised Mango Box <mechanised_mango_box@proton.me>
2025-05-18 01:22:47 +1000

    Update comments on RoomServer to be JsDoc compatible

--- code/beastly-brawl-showdown/server/room/RoomServer.js
+++ code/beastly-brawl-showdown/server/room/RoomServer.js
@@ -7,35 +7,35 @@ export class Player {
     this.displayName = displayName;
   }
 
-  // Method to get the name of this player
+  /** Method to get the name of this player */
   getName() {
     return this.displayName;
   }
 
-  // Method to display player name when player object is printed
+  /** Method to display player name when player object is printed */
   toString() {
     return this.displayName;
   }
 }
 
 export class Room {
-  // Preferences and Settings
+  /** Preferences and Settings */
   preferences = {};
-  // List of players
+  /** List of players */
   players = []; // Temporarily have no limit to the number of players
-  // Game state / History
+  /** Game state / History */
   gameState = {};
 
   constructor(roomCode) {
     this.roomCode = roomCode;
   }
 
-  // Method to add player to the list of players
+  /** Method to add player to the list of players */
   addPlayer(player) {
     this.players.push(player);
   }
 
-  // Method to get the id of this room
+  /** Method to get the id of this room */
   getRoomCode() {
     return this.roomCode;
   }
@@ -47,66 +47,67 @@ export class RoomServer {
   static CODE_ALPHABET = "0123456789";
 
   constructor(server_no, maxCapacity) {
-    // Ensure a minimum capacity
+    /**  Ensure a minimum capacity */
     const capacity = Math.max(RoomServer.MIN_CAPACITY, maxCapacity);
 
+    /** Server instance number */
     this.server_no = server_no;
-    // Number of currently active rooms
+    /**  Number of currently active rooms */
     this.numberOfRooms = 0;
-    // Current highest number of times a room has been used
+    /**  Current highest number of times a room has been used */
     this.currMax = 0;
-    // Index of the available room to be used next
+    /**  Index of the available room to be used next */
     this.availableRoom = 0;
-    // The array to store rooms
+    /**  The array to store rooms */
     this.rooms = new Array(capacity).fill(null);
-    // The array to store the number of times each room has been used
+    /**  The array to store the number of times each room has been used */
     this.uses = new Array(capacity).fill(0);
   }
 
-  // Create room
+  /** Create room */
   async createRoom() {
     if (this.isFull()) {
       throw new Error("Server is full");
     }
 
-    // Initialize new sqids object
+    /** Initialize new sqids object */
     const sqids = new Sqids({ minLength: RoomServer.CODE_MIN_LENGTH, alphabet: RoomServer.CODE_ALPHABET });
-    // Get the index of the room to be created
+    /** Get the index of the room to be created */
     const slotIndex = this.availableRoom;
-    // Get the number of uses for this room
+    /** Get the number of uses for this room */
     const roomUses = this.uses[slotIndex];
-    // Encode by [server number, room number, room no. of use]
+    /** Encode by [server number, room number, room no. of use] */
     const roomCode = sqids.encode([this.server_no, slotIndex, roomUses]);
-    // Create temporary room object with this room id
-    const room = new Room(roomCode); // Temporary implementation of room object
-    // Assign room to slot
+    /** Create temporary room object with this room id */
+    const room = new Room(roomCode); /** Temporary implementation of room object
+    /** Assign room to slot */
     this.rooms[slotIndex] = room;
-    // Increment the number of currently active rooms
+    /** Increment the number of currently active rooms */
     this.numberOfRooms++;
-    // Increment the number of uses of this room
+    /** Increment the number of uses of this room */
     this.uses[slotIndex]++;
 
-    // Update the currMax
+    /** Update the currMax */
     this.updateCurrentMax();
-    // Update the next available slot
+    /** Update the next available slot */
     this.updateAvailableSlot(slotIndex + 1);
 
     return room.getRoomCode();
   }
 
-  // Delete room
+  /** Delete room */
   async deleteRoom(roomCode) {
-    // Check if the room is empty
+    /** Check if the room is empty */
     if (this.isEmpty()) {
       throw new Error("Server is empty");
     }
 
-    // Check if there is a room with this code
+    /** Check if there is a room with this code */
     if (!this.hasInstanceWithCode(roomCode)) {
       throw new Error("Room not found");
     }
 
-    // Variable for the deleted index
+    /** Variable for the deleted index */
     let deletedIndex = null;
     // Find the room with this roomId
     for (let i = 0; i < this.length; i++) {
@@ -118,7 +119,7 @@ export class RoomServer {
       }
     }
 
-    // Safety check
+    // Safety check 
     if (deletedIndex === null) {
       throw new Error("Somehow bypassed the first error check");
     }
@@ -130,7 +131,7 @@ export class RoomServer {
     this.updateAvailableSlot(deletedIndex);
   }
 
-  // Update the current highest number of times a room has been used
+  /** Update the current highest number of times a room has been used  */
   updateCurrentMax() {
     let current = this.currMax;
     // Look for max
@@ -175,17 +176,17 @@ export class RoomServer {
     this.availableRoom = null; // No available slots
   }
 
-  // Returns true if no rooms available
+  /** Returns true if no rooms available */
   isFull() {
     return this.numberOfRooms === this.rooms.length || this.availableRoom === null;
   }
 
-  // Returns true if there are no rooms being used
+  /** Returns true if there are no rooms being used */
   isEmpty() {
     return this.numberOfRooms === 0;
   }
 
-  // Get number of rooms being used
+  /** Get number of rooms being used */
   get length() {
     return this.numberOfRooms;
   }
