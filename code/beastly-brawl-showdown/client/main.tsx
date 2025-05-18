8e765f8b5de6a1ef249f617b346db7f593f69f5e
Rohan Kannan <2rohankannan@gmail.com>
2025-05-18 14:24:41 +1000

    rebased onto develop

--- code/beastly-brawl-showdown/client/main.tsx
+++ code/beastly-brawl-showdown/client/main.tsx
@@ -13,6 +13,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "/imports/ui/App";
+import { Meteor } from "meteor/meteor";
 
 Meteor.startup(() => {
   const container = document.getElementById("react-target");
