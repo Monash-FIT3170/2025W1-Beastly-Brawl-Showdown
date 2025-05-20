15cb19e53d8aa1ae8d03c8b1dff6503ddf64be62
Rohan Kannan <2rohankannan@gmail.com>
2025-05-21 09:07:25 +1000

    fixed an error caused by typescript change

--- code/beastly-brawl-showdown/client/main.tsx
+++ code/beastly-brawl-showdown/client/main.tsx
@@ -1,9 +1,12 @@
-import { createRoot } from 'react-dom/client';
-import  { App }  from '../imports/ui/App';
-import { Meteor } from 'meteor/meteor';
+import { createRoot } from "react-dom/client";
+import { App } from "../imports/ui/App";
+import { Meteor } from "meteor/meteor";
 
 Meteor.startup(() => {
-  const container = document.getElementById('react-target');
+  const container = document.getElementById("react-target");
+  if (!container) {
+    throw new Error("React root container element not found.");
+  }
   const root = createRoot(container);
   root.render(<App />);
 });
