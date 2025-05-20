3870769633249b5632e3827a2c44bfea83d4c561
Jamie Kaltzis <jamiekaltzis02@gmail.com>
2025-05-21 08:45:09 +1000

    Remove unused static method perform

--- data/actions/AbilityAction.ts
+++ data/actions/AbilityAction.ts
@@ -12,14 +12,6 @@ export default class AbilityAction {
         this.defender = defender;
     }
 
-    /**
-     * Static method to perform the ability action outside of instance context.
-     * Calls useAbility on the attacker, passing the defender.
-     */
-    static perform(attacker: Monsters, defender: Monsters): void {
-        attacker.useAbility(defender);
-    }
-
     execute(): void {
         this.attacker.useAbility(this.defender);
     }
