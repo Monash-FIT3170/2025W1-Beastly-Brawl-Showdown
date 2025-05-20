3d1fb7afae57b8800b4551527e207b05947eac9c
Mark Shim Wen Ren <Mshi0073@student.monash.edu>
2025-05-20 18:23:56 +1000

    added guest name functionality to the homepage

--- code/beastly-brawl-showdown/imports/ui/HomePage.tsx
+++ code/beastly-brawl-showdown/imports/ui/HomePage.tsx
@@ -4,12 +4,18 @@ import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
   const navigate = useNavigate();
+  const guestName = sessionStorage.getItem("guestName")
+
+  const handleGuestHostName = () =>{
+    const safeGuestName = guestName ?? ""; // If null, use empty string, this gets rid of a red line on my ide
+    navigate(`/host/${encodeURIComponent(safeGuestName)}`)
+  };
 
   return (
     <div className="homepage-container">
       <div className="logo-container">Put logo here</div>
       <div className="buttons-container">
-        <button className="btn host-btn" onClick={() => navigate("/host")}>
+        <button className="btn host-btn" onClick={handleGuestHostName}>
           HOST
         </button>
         <button className="btn join-btn" onClick={() => navigate("/join")}>
