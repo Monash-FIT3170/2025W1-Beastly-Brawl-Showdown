1432b44aac32da7f8d8bf92235f7ef35e9aacf37
Mark Shim Wen Ren <Mshi0073@student.monash.edu>
2025-05-22 05:24:19 +1000

    idk what this is honestly I fixed 36 rebases

--- data/monsters/MysticWyvern.ts
+++ data/monsters/MysticWyvern.ts
@@ -25,19 +25,34 @@ export default class MysticWyvern extends Monsters {
     let roll = DiceRoller.d20();
     console.log(`${this.type} rolls ${roll}.`);
 
-        // Ask the user if they want to reroll
-        if (rerollCharge) {
-            var reroll = confirm("Do you want to reroll the d20?");
-        }
-        
-        if (reroll) {
-            roll = DiceRoller.d20();
-            console.log(`${this._type} rerolls and gets ${roll}.`);
-        }
-
-        const totalAttack = roll + this._attackBonus;
-        console.log(`${this._type} rolls ${roll}... Attack = ${totalAttack}.`);
-
-        defender.defend(totalAttack);
+    // Ask the user if they want to reroll, only if reroll charge is available
+    if (this.rerollCharge) {
+      // Note: In Node.js or non-browser environments, confirm won't work.
+      // You may want to replace with a different prompt method or remove this in non-browser context.
+      const reroll = confirm("Do you want to reroll the d20?");
+      if (reroll) {
+        this.rerollCharge = false;
+        roll = DiceRoller.d20();
+        console.log(`${this.type} rerolls and gets ${roll}.`);
+      }
     }
+
+    const totalAttack = roll + this.attackBonus;
+    console.log(`${this.type} rolls ${roll}... Attack = ${totalAttack}.`);
+
+    return totalAttack;
+  }
+
+  /**
+   * Uses elemental breath ability against the defender.
+   * Deals 15 + d10 damage.
+   * @param defender The monster being attacked.
+   */
+  useAbility(defender: Monsters): void {
+    console.log(`${this.type} uses elemental breath...`);
+    const roll = DiceRoller.d10();
+    const totalAttack = 15 + roll;
+    console.log(`${this.type} rolls ${roll}... Attack = ${totalAttack}.`);
+    defender.defend(totalAttack);
+  }
 }
