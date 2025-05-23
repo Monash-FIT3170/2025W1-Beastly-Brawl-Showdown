41a3b78f7ac213a55c1e922e71ec9589eee14352
Ryan Tan <geartown08@gmail.com>
2025-05-23 16:41:45 +1000

    Fixed some issues that were found in merge, got rid of unused variables

--- data/monsters/StoneHideGuardian.ts
+++ data/monsters/StoneHideGuardian.ts
@@ -4,8 +4,8 @@ import DiceRoller from '../utils/DiceRoller';
 export default class StoneHideGuardian extends Monsters {
     constructor() {
         super(30, 16, 1, "4 defense actions per battle", "Defender");
-        this._baseDefenseCharges = 4;
-        this._currentDefenseCharges = 4;
+        this.baseDefenseCharges = 4;
+        this.currentDefenseCharges = 4;
     }
 
     useAbility(defender: { stun: () => void }): void {
