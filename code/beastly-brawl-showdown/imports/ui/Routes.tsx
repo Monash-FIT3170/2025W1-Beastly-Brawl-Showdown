cac73139baffd3472704b8d17e118e3df603860b
Mechanised Mango Box <mechanised_mango_box@proton.me>
2025-05-25 02:32:47 +1000

    Reorganise routes

---
+++ code/beastly-brawl-showdown/imports/ui/Routes.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import { NotFound } from "./Error/NotFound";
+import { HomePage } from "./HomePage";
+import { Player } from "./PlayerPage";
+import { Room } from "./Room";
+import { HostRoomRequestPage } from "./host/HostRoomRequestPage";
+import { GuestNamePage } from "./GuestNamePage";
+import { LoginPage } from "./Login/LoginPage";
+import JoinPage from "./player/JoinPage";
+import ProjectorPage from "./host/ProjectorPage";
+
+export const renderRoutes = () => (
+  <>
+    {/* HOST */}
+    <Route path="/host/" element={<HostRoomRequestPage />} />
+    <Route path="/room/" element={<ProjectorPage />} />
+
+    {/* PLAYER */}
+    <Route path="/join/:joinCode" element={<JoinPage />} />
+    <Route path="/join/" element={<JoinPage />} />
+    <Route path="/play" element={<Player />} />
+
+    {/* HOME */}
+    <Route path="/home" element={<HomePage />} />
+    <Route path="/" element={<HomePage />} />
+
+    {/* DEFAULTS */}
+    <Route path="*" element={<NotFound />} />
+  </>
+);
