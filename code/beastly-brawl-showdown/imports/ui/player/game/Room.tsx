c8bc1cf0a1150ef9d28a0dc34981628464ee2d64
Joesiah <tsunetoishida03@gmail.com>
2025-05-26 23:22:15 +1000

    Fixed errors

--- code/beastly-brawl-showdown/imports/ui/player/game/Room.tsx
+++ code/beastly-brawl-showdown/imports/ui/player/game/Room.tsx
@@ -4,28 +4,28 @@ import { QRBox } from "../../host/projector/QRBox";
 import { CodeLink } from "../../host/projector/CodeLink";
 
 export const Room = () => {
-  const id = sessionStorage.getItem("roomId");
-
+  const joinCode = sessionStorage.getItem("roomId");
   //get name from session storage
-  const [revealURL, setURL] = useState('');
+  const playerName = sessionStorage.getItem("guestName");
+  const [revealURL, setURL] = useState("");
 
   useEffect(() => {
     if (joinCode) {
       const joinURL = Meteor.absoluteUrl(`/join/${joinCode}`);
       setURL(joinURL);
     }
-  }, [id]);
+  }, [joinCode]);
 
   return (
     <div className="waiting-room-box">
         <div className="waiting-room-info-box">
             <div className="room-code">
-                {id}
+                {joinCode}
             </div>
 
             {CodeLink(revealURL)}
             
-            <QRBox joinURL={revealURL} />
+            <QRBox joinUrl={revealURL} />
         </div>
 
         <div className="participants-display-box">
