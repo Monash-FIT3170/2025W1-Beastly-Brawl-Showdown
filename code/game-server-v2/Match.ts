0d54dce7dc974c3882108e3063f459ce4a501149
Harshil <iamharshilpatelfrommars@gmail.com>
2025-05-29 10:26:03 +1200

    Updated match to pass data once match starts to players

--- code/game-server-v2/Match.ts
+++ code/game-server-v2/Match.ts
@@ -1,13 +1,14 @@
 import { Player } from "./Player";
 import Monsters from "../beastly-brawl-showdown/imports/data/monsters/Monsters";
 import { Action } from "./types";
+import { Server } from "socket.io";
 
 export class Match {
   
   matchId: string;
   player1: Player;
   player2: Player;
-  // Need io but leave that to harshil to do
+  // Need io but leave that to harshil to do - done
 
   /**
    * Things that need to be stored in match states:
@@ -27,5 +28,25 @@ export class Match {
     this.matchId = matchId;
     this.playerMonsters.set(player1, player1.getMonster());
     this.playerMonsters.set(player2, player2.getMonster());
+  }
+
+  start(io: Server) {
+    const monster1 = this.playerMonsters.get(this.player1);
+    const monster2 = this.playerMonsters.get(this.player2);
+
+    const player1Data = {
+      matchId: this.matchId,
+      you: { socketId: this.player1.socketId, displayName: this.player1.displayName, monster: monster1 },
+      opponent: { socketId: this.player2.socketId, displayName: this.player2.displayName, monster: monster2 }
+    };
+
+    const player2Data = {
+      matchId: this.matchId,
+      you: { displayName: this.player2.displayName, monster: monster2 },
+      opponent: { displayName: this.player1.displayName, monster: monster1 }
+    };
+
+    io.to(this.player1.socketId).emit("match-start", player1Data);
+    io.to(this.player2.socketId).emit("match-start", player2Data);
   }
 }
