4ec4c7980ab0b1084b11b9fa1029bc4db820c301
Jonathan Foo <jonny.foo@outlook.com>
2025-05-30 15:05:56 +1000

    extra comments for later setup

--- code/beastly-brawl-showdown/imports/ui/BattleScreen/BattleScreen.tsx
+++ code/beastly-brawl-showdown/imports/ui/BattleScreen/BattleScreen.tsx
@@ -34,23 +34,50 @@ export const BattleScreen: React.FC<BattleScreenProps> = ({ selectedMonsterName 
     // Error checking for null socket
     if (!socket) return;
 
-
+    // Sets the monsters that battle when the game start is triggered
     const handleMatchStarted = (data: { enemyMonster: Monsters }) => {
       setenemyMonsterMonster(data.enemyMonster);
     };
 
     socket.on("match-started", handleMatchStarted);
 
+    //#region RECEIVE DICE AND ATTACK ANIMATIONS
+    //     let interval: NodeJS.Timeout;
+    // let timeout: NodeJS.Timeout;
+
+    // if (showAnimation) {
+    //   let i = 0;
+    //   const rollDuration = 1000; // total roll duration in ms
+    //   const intervalSpeed = 100; // time between number updates
+
+    //   const finalResult = 20; // eventually will replace with dice roll utility
+    //   const totalSteps = rollDuration / intervalSpeed; //get the ammount of times it gets swaped out
+
+    //   interval = setInterval(() => {
+    //     if (i < totalSteps) {
+    //       setDisplayedNumber(Math.floor(Math.random() * 20) + 1); // roll 1-20
+    //       i++;
+    //     } else {
+    //       clearInterval(interval);
+    //       setDisplayedNumber(finalResult);
+
+    //       timeout = setTimeout(() => {
+    //         console.log("Final result displayed for 3 seconds");
+    //       }, 3000);
+    //     }
+    //   }, intervalSpeed);
+    // }
+    //#endregion
   });
   //#endregion
 
-  //#region 
-  const triggerAnimation = () => {
-    if (!showAnimation) {
-      setShowAnimation(true);
-      setTimeout(() => setShowAnimation(false), 3000);
-    }
-  };
+  //#region Actions
+  // const triggerAnimation = () => {
+  //   if (!showAnimation) {
+  //     setShowAnimation(true);
+  //     setTimeout(() => setShowAnimation(false), 3000);
+  //   }
+  // };
 
   const handleAction = (action: 'attack' | 'defend' | 'ability') => {
     if (!socket) return;
@@ -60,8 +87,9 @@ export const BattleScreen: React.FC<BattleScreenProps> = ({ selectedMonsterName 
       action,
     });
 
-    triggerAnimation();
+    // triggerAnimation();
   };
+  //#endregion
 
   // HTML to show each monster and the animations
   return (
