41b292a2bc58b08235c0d710e58dc417de158af1
Jonathan Foo <jonny.foo@outlook.com>
2025-09-01 16:26:04 +1000

    Players page displays winner page when a winner is chosen

--- code/game-server-v2/tournament_manager.ts
+++ code/game-server-v2/tournament_manager.ts
@@ -41,7 +41,6 @@ export class TournamentManager {
     const winners = this.matches.map(m => m.winner!).filter(Boolean);
     if (winners.length === 1) {
       console.log(`Tournament Winner: ${winners[0].displayName}`);
-      // Optionally notify host:
       this.playerChannel.emit("tournament-finished", winners[0].displayName);
       return;
     }
@@ -57,7 +56,8 @@ export class TournamentManager {
     this.matches.forEach(match => match.createBattle());
 
     // Now run battles
-    await Promise.all(this.matches.map(match => match.runBattle(this.playerChannel)));
+    // await Promise.all(this.matches.map(match => match.runBattle(this.playerChannel)));
+    await(2000);
 
     // Check and start next round if needed
     this.checkRoundCompletion();
