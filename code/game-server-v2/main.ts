35826f31535514497a046fe7ab11eb0dcd9e1d84
Rohan Kannan <2rohankannan@gmail.com>
2025-09-02 12:47:29 +1000

    made volume for simulator so that game server can access it within its container

--- code/game-server-v2/main.ts
+++ code/game-server-v2/main.ts
@@ -10,11 +10,11 @@ import { log_attention, log_event, log_notice, log_warning } from "./utils";
 import * as fs from "fs";
 import * as path from "path";
 import { Player } from "./player";
-import { SideId } from "../beastly-brawl-showdown/imports/simulator/core/side";
-import { COMMON_MONSTER_POOL } from "../beastly-brawl-showdown/imports/simulator/data/common/common_monster_pool";
+import { SideId } from "/app/simulator/core/side";
+import { COMMON_MONSTER_POOL } from "/app/simulator/data/common/common_monster_pool";
 import { log } from "console";
-import { EntryID } from "../beastly-brawl-showdown/imports/simulator/core/utils";
-import { TargetingMethod } from "../beastly-brawl-showdown/imports/simulator/core/action/targeting";
+import { EntryID } from "/app/simulator/core/utils";
+import { TargetingMethod } from "/app/simulator/core/action/targeting";
 
 type ServerConfig = {
   serverIp: string;
