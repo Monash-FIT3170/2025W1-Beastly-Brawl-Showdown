7feddeb3815ae88ae166d83a60cdde74af4e8a7b
weihom05 <wtan0125@student.monash.edu>
2025-09-15 19:32:38 +1000

    Pop up for choosing different modes

--- code/beastly-brawl-showdown/imports/ui/HomePage.tsx
+++ code/beastly-brawl-showdown/imports/ui/HomePage.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { SelectMode } from "../SelectMode";
 
 export const HomePage = () => {
   const navigate = useNavigate();
+  const [showModal, setShowModal] = React.useState(false);
 
   const handleGuestHostName = () => {
     navigate(`/host/`);
+  };
+
+  //todo for the other type
+  const handleType2 = () => {
+    navigate("");
   };
 
   return (
@@ -13,7 +20,7 @@ export const HomePage = () => {
       <div className="homepage-container">
         <div className="logo"></div>
         <div className="buttons-container">
-          <button className="glb-btn" onClick={handleGuestHostName}>
+          <button className="glb-btn" onClick={() => setShowModal(true)}>
             HOST
           </button>
           <button className="glb-btn " onClick={() => navigate("/join")}>
@@ -21,6 +28,20 @@ export const HomePage = () => {
           </button>
         </div>
       </div>
+
+      <SelectMode
+        open={showModal}
+        onClose={() => setShowModal(false)}
+        onType1={() => {
+          setShowModal(false);
+          handleGuestHostName();
+        }}
+        //todo for the other mode
+        onType2={() => {
+          setShowModal(false);
+          handleType2();
+        }}
+      />
     </div>
   );
 };
