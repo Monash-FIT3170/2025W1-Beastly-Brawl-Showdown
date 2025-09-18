b3519763e49d3d6efced50f6c874827a7b309571
Rohan Kannan <2rohankannana@gmail.com>
2025-09-19 06:00:28 +1000

    should work now

--- code/game-server-v2/main.ts
+++ code/game-server-v2/main.ts
@@ -10,11 +10,11 @@ import { log_attention, log_event, log_notice, log_warning } from "./utils";
 import * as fs from "fs";
 import * as path from "path";
 import { Player } from "./player";
-import { SideId } from "../simulator/core/side";
-import { COMMON_MONSTER_POOL } from "../simulator/data/common/common_monster_pool";
+import { SideId } from "../beastly-brawl-showdown/imports/simulator/core/side";
+import { COMMON_MONSTER_POOL } from "../beastly-brawl-showdown/imports/simulator/data/common/common_monster_pool";
 import { log } from "console";
-import { EntryID } from "../simulator/core/utils";
-import { TargetingMethod } from "../simulator/core/action/targeting";
+import { EntryID } from "../beastly-brawl-showdown/imports/simulator/core/utils";
+import { TargetingMethod } from "../beastly-brawl-showdown/imports/simulator/core/action/targeting";
 import mongoose from "mongoose";
 
 export async function checkCollectionExists(collectionName: string): Promise<boolean> {
