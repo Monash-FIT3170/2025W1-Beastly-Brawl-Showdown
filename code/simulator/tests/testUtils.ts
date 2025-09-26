a04931668d3f00d6bb3f24b9ce3f6bd0c9b7c968
Harshil J Patel <hpat0037@student.monash.edu>
2025-09-27 11:55:40 +1200

    Added testUtils

---
+++ code/simulator/tests/testUtils.ts
@@ -0,0 +1,36 @@
+import { TargetingData } from "../core/action/targeting";
+import { SideId } from "../core/side";
+import { Notice } from "../core/notice/notice";
+import { BattleOptions, Battle } from "../core/battle";
+import { COMMON_MONSTER_POOL } from "../data/common/common_monster_pool";
+import { COMMON_MOVE_POOL } from "../data/common/common_move_pool";
+
+export function targetEnemy(side: number): TargetingData {
+  return { targetingMethod: "single-enemy", target: side as SideId };
+}
+
+export function makeBattle(seed: number): Battle {
+  const opts: BattleOptions = {
+    seed,
+    monsterPool: COMMON_MONSTER_POOL,
+    movePool: COMMON_MOVE_POOL,
+    playerOptionSet: [
+      { monsterId: "shadow_fang" },
+      { monsterId: "mystic_wryven" },
+    ],
+  };
+  return new Battle(opts);
+}
+
+export function autoResolveRollsAndRerolls() {
+  return {
+    onPostNotice: (_target: number, notice: Notice) => {
+      if (notice.kind === "roll") {
+        notice.callback();
+      } else if (notice.kind === "rerollOption") {
+        notice.callback(false);
+      }
+    },
+    onRemoveNotice: () => {},
+  };
+}
