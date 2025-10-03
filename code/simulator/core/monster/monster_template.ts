3fc9b4d296942f719951f5d5ae74e1eac1fac6c6
weihom05 <wtan0125@student.monash.edu>
2025-10-03 11:34:38 +1000

    Added the abilities from re balance branch so I can add new animations for new abilities

--- code/simulator/core/monster/monster_template.ts
+++ code/simulator/core/monster/monster_template.ts
@@ -7,8 +7,14 @@ import { MonsterStats } from "./monster_stats";
  * A base template for a monster
  */
 export type MonsterTemplate = {
+  /**
+   * The ID of this template
+   */
   templateId: MonsterId;
   //# Flavour
+  /**
+   * The display name (NOT ID).
+   */
   name: string;
   description: string;
   imageUrl: string;
@@ -19,7 +25,24 @@ export type MonsterTemplate = {
   //# Action
   attackActionId: EntryID;
   defendActionId: EntryID;
-  baseDefendActionCharges: number;
+  /**
+   * Number of attacks that can be performed
+   */
+  maxAttackCharges: number;
+  /** 
+   * Active ability id
+   * 
+   * - Optional
+   */
   abilityActionId?: EntryID;
+  /** 
+   * Actions to perform when spawning. 
+   * 
+   * This includes: 
+   * - Attaching passives as components
+   * - Initialising active ability charges as components
+   * - On spawn effects
+   */
   onSpawnActions: SpawnAction[];
 };
+
