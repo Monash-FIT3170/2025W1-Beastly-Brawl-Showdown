ea933323fb918fcd45d589106c0b08ad29166a44
weihom05 <wtan0125@student.monash.edu>
2025-10-04 08:14:41 +1000

    Remove the new monsters

--- code/simulator/data/common/common_monster_pool.ts
+++ code/simulator/data/common/common_monster_pool.ts
@@ -1,10 +1,9 @@
 import { Battle } from "../../core/battle";
-import { RerollChargeComponent, DodgeChargeComponent, AdvantageComponent, DamageReductionComponent, ThornsComponent } from "../../core/monster/component/core_components";
+import { RerollChargeComponent, DodgeChargeComponent} from "../../core/monster/component/core_components";
 import { MonsterPool } from "../../core/monster/monster_pool";
 import { SideId } from "../../core/side";
 
-type MONSTER_IDS = "blank" | "mystic_wryven" | "shadow_fang" | "stone_hide" | "fleet_foot" |
-  "knight" | "sea_urchin" | "lion" | "bear" | "shield";
+type MONSTER_IDS = "blank" | "mystic_wryven" | "shadow_fang" | "stone_hide";
 export const COMMON_MONSTER_POOL: MonsterPool<MONSTER_IDS> = {
   name: "common_monster_pool",
   monsters: {
@@ -70,6 +69,7 @@ export const COMMON_MONSTER_POOL: MonsterPool<MONSTER_IDS> = {
       defendActionId: "defend",
       maxAttackCharges: 3,
       abilityActionId: "dodge",
+      abilityName: "Dodge",
       onSpawnActions: [
         {
           type: "spawnAction",
@@ -97,146 +97,9 @@ export const COMMON_MONSTER_POOL: MonsterPool<MONSTER_IDS> = {
       defendActionId: "defend",
       maxAttackCharges: 4,
       abilityActionId: "stun",
+      abilityName: "Stun",
       onSpawnActions: [
 
-      ],
-    },
-
-    fleet_foot: {
-      templateId: "fleet_foot",
-      name: "Fleet Foot Stalker",
-      description: "A swift and elusive hunter. Excels in dealing multi-hit damage.",
-      imageUrl: "/assets/monsters/owl.png",
-      baseStats: {
-        health: 32,
-        armour: 8,
-        attack: 3,
-        speed: 4,
-        crit_chance: 7,
-        crit_damage: 7,
-      },
-      attackActionId: "attack-normal",
-      defendActionId: "defend",
-      maxAttackCharges: 3,
-      abilityActionId: "double-attack",
-      onSpawnActions: [],
-    },
-
-    knight: {
-      templateId: "knight",
-      name: "Knight",
-      description: "A brave and noble warrior. A Balanced Monster.",
-      imageUrl: "/assets/monsters/knight.png",
-      baseStats: {
-        health: 50,
-        armour: 10,
-        attack: 2,
-        speed: 4,
-        crit_chance: 5,
-        crit_damage: 2,
-      },
-      attackActionId: "attack-normal",
-      defendActionId: "defend",
-      maxAttackCharges: 3,
-      onSpawnActions: [
-        {
-          type: "spawnAction",
-          do: async function (world: Battle, source: SideId): Promise<void> {
-            world.sides[source].monster.components.push(new AdvantageComponent());
-          },
-        }
-      ],
-    },
-
-    sea_urchin: {
-      templateId: "sea_urchin",
-      name: "Sea Urchin",
-      description: "A prickly marine creature. A Defense Monster that deals damage back when hit.",
-      imageUrl: "/assets/monsters/sea_urchin.png",
-      baseStats: {
-        health: 60,
-        armour: 10,
-        attack: 0,
-        speed: 4,
-        crit_chance: 5,
-        crit_damage: 2,
-      },
-      attackActionId: "attack-normal",
-      defendActionId: "defend",
-      maxAttackCharges: 3,
-      onSpawnActions: [
-        {
-          type: "spawnAction",
-          do: async function (world: Battle, source: SideId): Promise<void> {
-            world.sides[source].monster.components.push(new ThornsComponent(1));
-          },
-        }
-      ],
-    },
-
-    lion: {
-      templateId: "lion",
-      name: "Lion",
-      description: "A fierce and majestic predator. An Attack Monster that excels in hitting hard.",
-      imageUrl: "/assets/monsters/lion.png",
-      baseStats: {
-        health: 36,
-        armour: 6,
-        attack: 5,
-        speed: 4,
-        crit_chance: 5,
-        crit_damage: 2,
-      },
-      attackActionId: "attack-normal",
-      defendActionId: "defend",
-      maxAttackCharges: 3,
-      abilityActionId: "attack-bonus-next3",
-      onSpawnActions: [],
-    },
-
-    bear: {
-      templateId: "bear",
-      name: "Bear",
-      description: "A strong and resilient creature. A Balanced Monster that goes berserk.",
-      imageUrl: "/assets/monsters/bear.png",
-      baseStats: {
-        health: 50,
-        armour: 9,
-        attack: 2,
-        speed: 4,
-        crit_chance: 5,
-        crit_damage: 2,
-      },
-      attackActionId: "attack-normal",
-      defendActionId: "defend",
-      maxAttackCharges: 3,
-      abilityActionId: "battle-cry",
-      onSpawnActions: [],
-    },
-
-    shield: {
-      templateId: "shield",
-      name: "Shield",
-      description: "A shield that once belonged to a knight gained sentience. A defender monster with inate damage reduction.",
-      imageUrl: "/assets/monsters/shield.png",
-      baseStats: {
-        health: 60,
-        armour: 12,
-        attack: 1,
-        speed: 4,
-        crit_chance: 5,
-        crit_damage: 2,
-      },
-      attackActionId: "attack-normal",
-      defendActionId: "defend",
-      maxAttackCharges: 3,
-      onSpawnActions: [
-        {
-          type: "spawnAction",
-          do: async function (world: Battle, source: SideId): Promise<void> {
-            world.sides[source].monster.components.push(new DamageReductionComponent(3));
-          },
-        }
       ],
     },
   },
