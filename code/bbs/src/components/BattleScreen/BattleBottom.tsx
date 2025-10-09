86aa73dfb2ff9ed392725749b990584ec279db46
Ryan Tan <geartown08@gmail.com>
2025-10-09 14:23:11 +1100

    Fixed BattleBottom with missing brackets

--- code/bbs/src/components/BattleScreen/BattleBottom.tsx
+++ code/bbs/src/components/BattleScreen/BattleBottom.tsx
@@ -3,21 +3,15 @@ import { type EntryID } from "../../../../simulator/core/utils";
 import { type TargetingMethod } from "../../../../simulator/core/action/targeting";
 import { COMMON_MOVE_POOL } from "../../../../simulator/data/common/common_move_pool";
 
-type Button = {
-  id: string;
-  icon: string;
-}
-
-type MoveButton = Button & {
+type MoveButton = {
   id: EntryID;
+  icon: string;
   targetMethod: TargetingMethod;
 };
 
 type BattleBottomProps = {
   onAction: (moveId: EntryID, targetMethod: TargetingMethod) => void;
   disabled?: boolean;
-  onRoll: () => void;
-  mode: "combat" | "roll";
   myMonsterMoves: {
     attack: EntryID;
     ability?: EntryID;
@@ -29,10 +23,7 @@ export const BattleBottom: React.FC<BattleBottomProps> = ({
   onAction,
   disabled,
   myMonsterMoves,
-  onRoll,
-  mode,
-}: BattleBottomProps) => {
-
+}) => {
   const getMove = (moveId: EntryID) => {
     return (
       COMMON_MOVE_POOL as Record<
@@ -50,24 +41,18 @@ export const BattleBottom: React.FC<BattleBottomProps> = ({
     } as MoveButton;
   };
 
-  //added a button that isn't tied to the monsters actions
-  const buildNormalButton = (id: string, fallbackIcon: string) => {
-    return {
-      id: id,
-      icon: fallbackIcon,
-    } as Button;
-  };
-
   // Build button configs dynamically
-  const attackBtn = buildButton(myMonsterMoves.attack, "assets/battle-icons/sword3.png");
-  const defendBtn = buildButton(myMonsterMoves.defend, "assets/battle-icons/shield2.png");
+  const attackBtn = buildButton(
+    myMonsterMoves.attack,
+    "/assets/img/sword3.png"
+  );
+  const defendBtn = buildButton(
+    myMonsterMoves.defend,
+    "/assets/img/shield2.png"
+  );
   const abilityBtn = myMonsterMoves.ability
-    ? buildButton(
-      myMonsterMoves.ability,
-      "assets/img/ability2.png"
-    )
+    ? buildButton(myMonsterMoves.ability, "/assets/img/ability2.png")
     : null;
-  const rollBtn = buildNormalButton("roll","/assets/img/d20.png")
 
   const renderButton = (btn: MoveButton) => (
     <button
@@ -76,30 +61,20 @@ export const BattleBottom: React.FC<BattleBottomProps> = ({
       onClick={() => onAction(btn.id, btn.targetMethod)}
       disabled={disabled}
     >
-      <img src={btn.icon} className="battleScreenBottomButtonImage" />
-    </button>
-  );
-
-    const renderButtonForRoll = (btn: Button) => (
-    <button
-      key={btn.id}
-      className="glb-btn"
-      onClick={() => onRoll()}
-    >
-      <img src={btn.icon} alt={btn.id} className="battleScreenBottomButtonImage" />
+      <img
+        src={btn.icon}
+        alt={btn.id}
+        className="battleScreenBottomButtonImage"
+      />
     </button>
   );
 
   return (
-<div className="battleScreenBottom">
-  {mode === "roll" ? (
-    renderButtonForRoll(rollBtn)
-  ) : (
-    <>
+    <div className="battleScreenBottom">
       {renderButton(attackBtn)}
       {abilityBtn && renderButton(abilityBtn)}
       {renderButton(defendBtn)}
-      {/* <div className="shield-uses"></div> */}
+      <div className="shield-uses"></div>
     </div>
   );
 };
