be3bc465a750663093953ac38add0d92d77c40f9
Rohan Kannan <2rohankannana@gmail.com>
2025-10-20 21:07:34 +1100

    potential fix to fix front end deployment

--- code/bbs/vite.config.ts
+++ code/bbs/vite.config.ts
@@ -3,10 +3,17 @@ import react from "@vitejs/plugin-react";
 import { env } from "process";
 
 // https://vite.dev/config/
+const PORT = Number(process.env.PORT) || 5173;
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: Number(env.PORT) ?? 5173,
+    port: PORT,
     host: true,
-  },
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'two025w1-beastly-brawl-showdown-frontend.onrender.com'
+    ]
+  }
 });
