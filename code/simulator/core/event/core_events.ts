53c92293bac8e1fd0564da1c6c43d8e82c0ae600
Ryan <geartown08@gmail.com>
2025-10-22 15:04:20 +1100

    changed it to use move.name instead of move.moveCat

--- code/simulator/core/event/core_events.ts
+++ code/simulator/core/event/core_events.ts
@@ -50,7 +50,7 @@ export interface MoveSuccessEvent extends BaseEvent {
   source: SideId;
   target: SideId;
   moveId: EntryID;
-  moveCat?: string;
+  moveName?: string;
 }
 
 export interface MoveEvadedEvent extends BaseEvent {
@@ -64,7 +64,7 @@ export interface MoveFailedEvent extends BaseEvent {
   source: SideId;
   target: SideId;
   moveId: EntryID;
-  moveCat?: string;
+  moveName: string;
   reason: unknown; // TODO
 }
 
@@ -72,5 +72,5 @@ export interface BuffEvent extends BaseEvent {
   name: "buff";
   source: SideId;
   target: SideId;
-  buffs: {attack?: number, armour?: number };
+  buffs: { attack?: number, armour?: number };
 }
