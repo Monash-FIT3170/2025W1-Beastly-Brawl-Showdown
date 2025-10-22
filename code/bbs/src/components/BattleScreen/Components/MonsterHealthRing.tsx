3c227b62310764c4818f9d08321747add939b570
weihom05 <wtan0125@student.monash.edu>
2025-10-22 22:43:04 +1100

    Made some changes to the health ring resize

--- code/bbs/src/components/BattleScreen/Components/MonsterHealthRing.tsx
+++ code/bbs/src/components/BattleScreen/Components/MonsterHealthRing.tsx
@@ -40,13 +40,8 @@ const MonsterHealthRing: React.FC<Props> = ({
 }) => {
   const [showTooltip, setShowTooltip] = useState(false);
 
-  const size = 170; // circle diameter
-  const stroke = 17; // thickness of ring (approximate 8-10% of size)
-  const radius = size / 2 - stroke / 2;
-  const circumference = 2 * Math.PI * radius;
-
   const percent = Math.max(0, Math.min(1, currentHealth / maxHealth));
-  const dashOffset = circumference * (1 - percent);
+  // const dashOffset = circumference * (1 - percent);
 
   const healthClass =
     percent >= 0.7
@@ -60,8 +55,7 @@ const MonsterHealthRing: React.FC<Props> = ({
       className="health-ring-container"
       style={
         {
-          "--ring-size": `${size}px`,
-          "--ring-stroke": `${stroke}px`,
+          "--health-percent": percent,
         } as React.CSSProperties
       }
       //desktop uses hover
@@ -72,17 +66,9 @@ const MonsterHealthRing: React.FC<Props> = ({
       onTouchEnd={() => setShowTooltip(false)}
       onTouchCancel={() => setShowTooltip(false)}
     >
-      <svg className="health-ring" width={size} height={size}>
-        <circle className="ring-bg" cx={size / 2} cy={size / 2} r={radius} />
-        <circle
-          className={`ring-fg ${healthClass}`}
-          cx={size / 2}
-          cy={size / 2}
-          r={radius}
-          strokeWidth={stroke}
-          strokeDasharray={circumference}
-          strokeDashoffset={dashOffset}
-        />
+      <svg className="health-ring">
+        <circle className="ring-bg" />
+        <circle className={`ring-fg ${healthClass}`} />
       </svg>
       <img src={imageSrc} alt="monster" className="monster-img" />
 
