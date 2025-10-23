4381358fb7d9163b4957477bad72650954a97445
Rohan Kannan <2rohankannana@gmail.com>
2025-10-23 18:12:56 +1100

    hardcoding server connects, but nothing loads

--- code/bbs/src/utils/RoomMethods.ts
+++ code/bbs/src/utils/RoomMethods.ts
@@ -42,8 +42,7 @@ export const getBestServerUrl = async (): Promise<string> => {
       // if (serverUrl.trim().length > 0) {
       //   resolve(serverUrl);
       // }
-      // resolve("https://two025w1-beastly-brawl-showdown.onrender.com")
-        resolve("https://two025w1-beastly-brawl-showdown-1.onrender.com")
+      resolve("https://two025w1-beastly-brawl-showdown.onrender.com")
       reject(new Error("Room is not joinable."));
     });
   } catch (e) {
